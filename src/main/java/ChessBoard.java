d95557bf959685cee6b553a61fb6858149f7ef27
Vladimir <milliganb43@gmail.com>
2026-03-19 12:14:35 +0300

    Two-dimensional array

--- src/main/java/ChessBoard.java
+++ src/main/java/ChessBoard.java
@@ -20,7 +20,8 @@ public class ChessBoard {
             for (int j = 0; j < 8; j++) {
                 System.out.print(chessBoard[i][j] + " ");
             }
-            System.out.println(); // Переход на новую строку после каждой строки доски
+            System.out.println();
+            // Переход на новую строку после каждой строки доски
         }
     }
 }
