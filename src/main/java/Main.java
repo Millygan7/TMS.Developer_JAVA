f3ce59b276eecc835d00bc7ccb23a85b68b756f9
Vladimir <milliganb43@gmail.com>
2026-03-30 18:41:38 +0300

    Change method Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,44 @@
-public class Main {
-}
-public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    Animal rabbit = new Rabbit("трава", " мясо");
-    Animal dog = new Dog("Бобрик","ss");
-    Animal tiger =  new Tiger("ss","ss");
-    System.out.println("=== Звуки животных ===");
-   dog.voice();
-    tiger.voice();
-    rabbit.voice();
+/**
+ @author
+ Pleshakov Vladimir
+ *@date 28.03.2026
+ *@description OOP an Java.Polimorfism and static methods.
+ */
 
-    System.out.println("\n=== Питание ===");
 
-    // Собака
-    dog.eat("Grass");
-    dog.eat("Meat");
-    dog.eat("Cheese");
 
-    System.out.println();
 
-    // Тигр
-    tiger.eat("Grass");
-    tiger.eat("Meat");
-    tiger.eat("Fish");
-
-    System.out.println();
-
-    // Кролик
-    rabbit.eat("Grass");
-    rabbit.eat("Meat");
-    rabbit.eat("Carrot");
-}
+//public class Main {
+//}
+//public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//    Animal rabbit = new Rabbit("трава", " мясо");
+//    Animal dog = new Dog("Бобрик","ss");
+//    Animal tiger =  new Tiger("ss","ss");
+//    System.out.println("=== Звуки животных ===");
+//   dog.voice();
+//    tiger.voice();
+//    rabbit.voice();
+//
+//    System.out.println("\n=== Питание ===");
+//
+//     Собака
+//    dog.eat("Grass");
+//    dog.eat("Meat");
+//    dog.eat("Cheese");
+//
+//    System.out.println();
+//
+//     Тигр
+//    tiger.eat("Grass");
+//    tiger.eat("Meat");
+//    tiger.eat("Fish");
+//
+//    System.out.println();
+//
+//     Кролик
+//    rabbit.eat("Grass");
+//    rabbit.eat("Meat");
+//    rabbit.eat("Carrot");
+//}
 
