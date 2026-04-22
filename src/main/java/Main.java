8ae647edc431d861b748876b133e6764b907e7f6
Vladimir <milliganb43@gmail.com>
2026-04-22 12:15:44 +0300

    Pattern And Matcher

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 //public class Main {
-}
+
 
 
 
@@ -36,8 +36,8 @@ import java.util.regex.Pattern;
 //
 //public static void main(String[] args)
 
-
-
+//
+//
 
 
 
