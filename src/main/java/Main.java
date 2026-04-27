064d916cfa5ce6ad4216cc9bf209c547e1eb3a66
Vladimir <milliganb43@gmail.com>
2026-04-27 18:16:39 +0300

    Change Main.Lesson_10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,21 @@
 //Создать класс для описания системы пользователя. Переопределить в классных методах
 //toString, хэш-код и равно. Создайте несколько экземпляров класса с одним и тем же.
 //значение полей и сравнение с помощью метода Equals.
-//public class Main {
-//   public static void main(String[] args) {
-//        SystemUser systemUser = new SystemUser("Danny", 123455667, 925100232);
-//        SystemUser systemUser1 = new SystemUser("Danny", 123455667, 925100232);
-//        System.out.println("systemUser .equals(systemUser1)? " + systemUser.equals(systemUser1));
-//        System.out.println(systemUser1.hashCode() == systemUser.hashCode());
-//        System.out.println(systemUser);
-//
-//    }
+public class Main {
+   public static void main(String[] args) {
+        SystemUser systemUser = new SystemUser("Danny", 123455667, 925100232);
+        SystemUser systemUser1 = new SystemUser("Danny", 123455667, 925100232);
+        System.out.println("systemUser .equals(systemUser1)? " + systemUser.equals(systemUser1));
+        System.out.println(systemUser1.hashCode() == systemUser.hashCode());
+        System.out.println(systemUser);
+
+    }}
 //                                    Задача *:
 //Создать программу для создания поверхностного и глубокого клонирования объектов
 //класса Пользователь. Пусть при входе в программу будет задействован тип операции клонирования
 //        (поверхностное клонирование или глубокое), а также id юзера для клонирования
 public static void main(String[] args) {
-    User user = new User("Danny", "derley63@yandex.ru", 9999241);
+ User user = new User("Danny", "derley63@yandex.ru", 9999241);
     System.out.println("Данные до клонирования :" + user.getUsername() + "  " + user.getEmail() + " " + user.getID() );
     User clone;
     try{
@@ -29,11 +29,9 @@ public static void main(String[] args) {
         clone = user.setUsername("Milligan");
         clone = user.setID(12323);
         clone = user.setEmail("fcMU.com");
-        System.out.println("Клон после изменения данные: " +
+        System.out.println("клон после изменения данные: " +
                 clone.getUsername() + " - " + clone.getID() + "ID");
     } catch (CloneNotSupportedException e) {
         throw new RuntimeException(e);
     }
-
-
-}
+    }
