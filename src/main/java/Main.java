c084317f9db0dc71b4d59b3890f604fc9eccec86
Vladimir <milliganb43@gmail.com>
2026-05-04 18:23:03 +0300

    Excecizes Exception

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 
 
 public class Main {
-
+}
 
     public static void main(String[] args) {
         VerificationUserInput verificationUserInput1 = new VerificationUserInput();
         VerificationUserInput.verification();
     }
-}
+
 
 
 
