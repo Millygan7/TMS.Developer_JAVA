b02ea3d2c3ae24caf625ce0de52c2db54eb0ecdf
Vladimir <milliganb43@gmail.com>
2026-05-05 20:03:55 +0300

    Change assignature

--- src/main/java/Exercize1.java
+++ src/main/java/Exercize1.java
@@ -4,18 +4,18 @@ import java.util.List;
 import java.util.Map;
 
 public class Exercize1 {
-    private static List<String> words = new ArrayList<>();
+    private static List<String> word = new ArrayList<>();
 
-    public Exercize1(List<String> words) {
-        this.words = words;
+    public Exercize1(List<String> word) {
+        this.word = word;
     }
 
     public List<String> getWord() {
-        return words;
+        return word;
     }
 
-    public void setWord(List<String> words) {
-        this.words = words;
+    public void setWord(List<String> word) {
+        this.word = word;
     }
 
     public static class DuplicateChecker {
