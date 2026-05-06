c1c28fa202893d555d7670d74dd2a629a0c02150
Vladimir <milliganb43@gmail.com>
2026-05-06 11:44:45 +0300

    Pattern And Matcher

---
+++ src/test/java/Main.java
@@ -0,0 +1,35 @@
+/**
+ * @author Pleshakov Vladimir
+ * @date 08.04.2026
+ * @description Pattern and Matcher
+ */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+//public class Main {
+//}
+
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите регулярное выражение: ");
+        String regex = scanner.nextLine();
+        System.out.print("Введите строку для поиска: ");
+        String input = scanner.nextLine();
+
+
+
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        boolean found = Pattern.matches("\\D{2,6}", input);
+        if(found)
+            System.out.println("Найдено");
+        else
+            System.out.println("Не найдено");
+    }
+
+
